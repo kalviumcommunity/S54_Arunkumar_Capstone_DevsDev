c0c84ff7475e2a97f7d19ce71aabc7cc16a7d519
sak23042006 <s.arunkumar567567567@gmail.com>
2024-03-18 13:19:07 +0530

    bug-fix

--- server/handler.js
+++ server/handler.js
@@ -1,9 +1,22 @@
 // homeHandler (get)
 
 const homeHandler = async(req,res)=>{
-    res.send("Welcome to Dev'sDev")
+    try {
+        res.send("Welcome to Dev'sDev")
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+const readData = async(req,res)=>{
+    try {
+        res.send("Read API Successfully")
+    } catch (error) {
+        res.status(500).json(error)
+    }
 }
 
 module.exports = {
-    homeHandler
+    homeHandler,
+    readData,
 }
