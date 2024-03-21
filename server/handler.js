f02800092d088c1ae07df5d50078b9c1d8553b02
sak23042006 <s.arunkumar567567567@gmail.com>
2024-03-21 12:50:41 +0530

    made a connection with the database

--- server/handler.js
+++ server/handler.js
@@ -1,40 +1,49 @@
 // homeHandler (get)
 const homeHandler = async(req,res)=>{
+
     try {
         res.send("Welcome to Dev'sDev")
     } catch (error) {
         res.status(500).json(error)
     }
+
 }
 
-// Reading API 
+// Reading API
 const readData = async(req,res)=>{
+
     try {
         res.status(200).send("Read API Successfully")
     } catch (error) {
         res.status(500).json(error)
     }
+
 }
 
 //Creating Data
 const createData = async(req,res)=>{
+
     try {
         res.status(201).send("Successfully posted using post request")
     } catch (error) {
         res.status(500).json(error)
     }
+    
 }
 
 // Updating Data  
 const updateData = async(req,res)=>{
+
     try {
         res.status(200).send("Data updated Successfully")
     } catch (error) {
         res.status(500).json(error)
     }
+
 }
 
 module.exports = {
+
     homeHandler,
     readData,
     createData,
